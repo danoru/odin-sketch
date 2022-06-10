48046be094717f81098b8854cab11dff8d3992ef
Daniel Mendez <awilddanoru@gmail.com>
2022-06-10 13:41:19 -0700

    Added button with on click for grid sizing

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const container = document.getElementById("container");
 
 function createGrid(rows, columns) {
   container.style.setProperty('--grid-rows', rows);
-  container.style.setProperty('--grid-cols', columns);
+  container.style.setProperty('--grid-columns', columns);
   for (c = 0; c < (rows * columns); c++) {
     let cell = document.createElement("div");
     cell.innerText = (c + 1);
@@ -14,5 +14,3 @@ function makeGrid() {
   let gridSize = prompt("What size grid would you like?")
   createGrid(gridSize, gridSize);
 }
-
-makeGrid();
