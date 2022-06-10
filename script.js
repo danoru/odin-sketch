c479059bc8a2002fc4de0d648d84510797c53175
Daniel Mendez <awilddanoru@gmail.com>
2022-06-10 07:32:21 -0700

    Added functionality to allow user to determine grid size

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 const container = document.getElementById("container");
 
-function makeRows(rows, cols) {
+function createGrid(rows, columns) {
   container.style.setProperty('--grid-rows', rows);
-  container.style.setProperty('--grid-cols', cols);
-  for (c = 0; c < (rows * cols); c++) {
+  container.style.setProperty('--grid-cols', columns);
+  for (c = 0; c < (rows * columns); c++) {
     let cell = document.createElement("div");
     cell.innerText = (c + 1);
     container.appendChild(cell).className = "grid-item";
   };
 };
 
-makeRows(16, 16);
+function makeGrid() {
+  let gridSize = prompt("What size grid would you like?")
+  createGrid(gridSize, gridSize);
+}
+
+makeGrid();
